5c0116a86b074a9d8cab93ce8874998f1bb7b43b
Jesus Garcia Loyola <116201865+JesusG2022@users.noreply.github.com>
2025-10-29 14:36:36 +0000

    I think I fix the error:

--- src/test/java/project3/com/example/rest_service/RestServiceApplicationTests.java
+++ src/test/java/project3/com/example/rest_service/RestServiceApplicationTests.java
@@ -2,12 +2,14 @@ package project3.com.example.rest_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles; // added
 
 @SpringBootTest
+@ActiveProfiles("test") // added
 class RestServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
