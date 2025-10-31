1824f39524303c37c8eac55183b540213fa5335d
Austin Metke <=>
2025-10-31 14:24:35 -0700

    Made H2 a test only dependency

--- src/test/java/project3/com/example/rest_service/HomeControllerTest.java
+++ src/test/java/project3/com/example/rest_service/HomeControllerTest.java
@@ -3,20 +3,20 @@ package project3.com.example.rest_service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(HomeController.class)
+@SpringBootTest
+@AutoConfigureMockMvc(addFilters = false)
 class HomeControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
-    @AutoConfigureMockMvc(addFilters = false)
     void homeReturnsWelcomeMessage() throws Exception {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
