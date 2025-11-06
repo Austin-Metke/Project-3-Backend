590daca1d4c1454bfbb0e71f33d92840aa31cee0
Austin Metke <=>
2025-11-06 12:16:13 -0800

    FIx Main test

--- src/test/java/project3/com/example/rest_service/RestServiceApplicationTests.java
+++ src/test/java/project3/com/example/rest_service/RestServiceApplicationTests.java
@@ -2,13 +2,8 @@ package project3.com.example.rest_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-<<<<<<< HEAD
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
-=======
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
->>>>>>> main
 
 @SpringBootTest
 @ActiveProfiles("test")
