acb6254b0deeeb337df696bf42207a2bf2a982fb
AlbertoCsumb <Albrodriguez@csumb.edu>
2025-11-17 13:21:02 -0800

    allowed connection to frontend with vite.

--- src/main/java/project3/com/example/rest_service/WebConfig.java
+++ src/main/java/project3/com/example/rest_service/WebConfig.java
@@ -9,9 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*") // change to specific origins in production
+            .allowedOrigins("http://localhost:5173")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
             .allowedHeaders("*")
-            .allowCredentials(false);
+            .allowCredentials(true);
     }
 }
